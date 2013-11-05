5649a34c1e109821ec5d51c55b909cd550346691
jonniedarko <jonnie.maguire@gmail.com>
2013-11-05 15:34:13 +0000

    Update app.js
    added global method to Format date for application use

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,15 @@
 'use strict';
 
-angular.module('zenApp', ['ui.bootstrap']);
+var app = angular.module('zenApp', ['ui.bootstrap']);
+
+app.run(function ($rootScope){
+	$rootScope.formatDate = function(date) {
+			var date_num = date.getDate() < 10 ? '0'+date.getDate() : date.getDate();
+			var day_num = date.getDay();
+			var month_num = (date.getMonth()+1) < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1;//date.getMonth();
+			var fullYear_num =  date.getFullYear();
+			//return days_of_week[day_num]+", "+date_num+ " of "+months[month_num]+" "+fullYear_num;
+			return fullYear_num+"-"+month_num+"-"+date_num;
+	
+		}
+	});
