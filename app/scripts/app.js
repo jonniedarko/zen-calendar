acc71608ed5bddf3a3c8f10bb57ab888077b735f
jonniedarko <jonnie.maguire@gmail.com>
2013-11-06 16:45:26 +0000

    Update app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,6 +2,22 @@
 
 var app = angular.module('zenApp', ['ui.bootstrap']);
 
+app.config(function ($provide) {
+	$provide.decorator('$controller', function ($delegate, tempUserAuthenticationFactory) {
+		return function (constructor, locals) {
+			var days_of_week = ["Sun","Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+			var months = ['January', 'February', 'March', 'April','May', 'June', 
+					'July', 'August', 'September','October', 'November', 'December'];
+			var number_of_days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+			locals.$scope.days_of_week = days_of_week;
+			locals.$scope.months = months;
+			locals.$scope.number_of_days_in_month=number_of_days_in_month;
+			locals.$scope._UserService = tempUserAuthenticationFactory;
+			return $delegate(constructor, locals);
+		};
+	});
+});
+
 app.run(function ($rootScope){
 	$rootScope.formatDate = function(date) {
 			var date_num = date.getDate() < 10 ? '0'+date.getDate() : date.getDate();
