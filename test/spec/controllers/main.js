069be62a25f16dc061e934deab70c046acc4eb83
John Maguire <jonnie.maguire@gmail>
2013-11-08 10:36:05 +0000

    Fixed issue with unit tests:loading mocked ui.bootstrap, using $templateCache in directive tests

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-/*describe('Controller: MainCtrl', function () {
+describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('calendarApp'));
+  beforeEach(module('zenApp'));
 
   var MainCtrl,
     scope;
@@ -11,13 +11,87 @@
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    MainCtrl = $controller('zenCalendarController', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+  it('Calendar Directive function Tests', function() {
+        var date = new Date('2013-10-27');
+        console.log('Testing with date = ' + date);
+        //var formattedDate = scope.formateDate(date);
+        //console.log('date = '+date);
+        //console.log('Formatted: '+);
+
+        expect(scope.getDayOfWeekIndex(date)).toBe(0);
+        //expect(scope.testMe()).toBe('testMe');
+        expect(scope.formatDate(date)).toBe('2013-10-27');
+        var first = scope.getFirstDateOfMonth(date);
+        //console.log('first= '+scope.formatDate(first));
+        var formatted_first = scope.formatDate(first);
+        console.log('formatted_first ' + formatted_first);
+        expect(formatted_first).toBe('2013-10-01');
+
+
+        var lastDay = scope.formatDate(scope.getLastDateOfMonth(date));
+        expect(lastDay).toBe('2013-10-31');
+
+
+        var perviousLastDay = scope.formatDate(scope.getLastDatePerviousMonth(date));
+        expect(perviousLastDay).toBe('2013-09-30');
+
+        var nextMonthsFirstDate = scope.formatDate(scope.getFirstDateNextMonth(date));
+        expect(nextMonthsFirstDate).toBe('2013-11-01');
+
+
+        var isToday = scope.isToday(new Date());
+        expect(isToday).toBe(true);
+
+
+    });
+
+    it('Calendar: getPerviousMonthDays function Tests', function() {
+
+        var date = new Date('2013-10-27');
+        var days = [];
+        days = scope.getPerviousMonthDays(date);
+
+
+        expect(days.length).toBe(2);
+
+    });
+
+    it('Calendar: getNextMonthDays function Tests', function() {
+
+        var date = new Date('2013-10-27');
+        var days = [];
+        days = scope.getNextMonthDays(date);
+      
+
+        expect(days.length).toBe(2);
+
+    });
+
+    it('Calendar: getDaysToRender function Tests', function () {
+
+        var date = new Date('2013-10-27');
+        var days = [];
+        days = scope.getDaysToRender(date);
+        
+
+        expect(days.length).toBe(35); 
+    });
+
+    it('Calendar: setCalendarDates function Tests', function () {
+
+        var date = new Date('2013-10-27');
+        var days = [];
+        scope.setCalendarDates(date);
+        days = scope.dates;
+        expect(days.length).toBe(5); 
+        expect(days[1].length).toBe(7); 
+    });
+
+
+
 });
-*/
